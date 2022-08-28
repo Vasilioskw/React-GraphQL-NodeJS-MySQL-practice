1335b942099a1980fcc20786926fb3f283a87df9
Vasilios Kambouras <Vasilioskw@gmail.com>
2022-08-27 21:16:06 -0400

    working on index.ts

--- CREM-back-end-practice/sever/scr/index.ts
+++ CREM-back-end-practice/sever/scr/index.ts
@@ -1,6 +1,13 @@
-import express from 'express';
+import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import cors from 'cors';
-import { createConnection } from "typeorm"
+import { DataSource } from "typeorm";
 
+const main = async () => {  
+    const app = express() // Creates an instance of express and allows middleware (including graphql)
+    app.use(cors()) // Middleware fro back to front end
+};
 
+main().catch((err) => {
+    console.log(err);
+}); 
