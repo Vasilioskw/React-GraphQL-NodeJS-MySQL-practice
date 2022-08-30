7e6d4ffc4363128b7382632e1f27b16852453ef1
Vasilios Kambouras <Vasilioskw@gmail.com>
2022-08-29 23:16:30 -0400

    working on Mysql setup.. need to fix passwprd or recreate server

--- CREM-back-end-practice/server/src/index.ts
+++ CREM-back-end-practice/server/src/index.ts
@@ -2,23 +2,34 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import cors from "cors";
 // import * as "typeorm";
-// import { DataSource } from "typeorm";
+import { DataSource } from "typeorm";
 
+const myDataSource = new DataSource({
+    type: "mysql",
+    database: "GraphqlCRUD",
+    username: "root",
+    password: "root",
+    logging: true,
+    // synchonize: false,
+    entities: []
+})
 
+const main = async () => { 
 
-const main = async () => {  
+    await myDataSource.initialize()
+        .then(() => {
+            console.log("Data Source has been initialized!")
+        })
+
+    
     const app = express() // Creates an instance of express and allows middleware (including graphql)
     app.use(cors()) // Middleware fro back to front end
     app.use(express.json()) // This just means, we will be using json as the body for every request to server (Which makes it easy for graphql)
    
-   
     // app.use("/graphql", graphqlHTTP({
     //     schema,
     //     graphql: true
-
-    // }))
-    // We will uncomment when this is defined
-
+    // })) // We will uncomment when this is defined
 
     console.log("index.ts is being read!")
 
@@ -27,6 +38,9 @@ const main = async () => {
     });
 };
 
+
 main().catch((err) => {
     console.log(err);
 });
+
+export default myDataSource;
